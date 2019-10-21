8187790f4cd0495046cbb51ff1e3f6276c57cfa6
shuto-san <shuemon2014@outlook.jp>
2019-10-21 22:39:23 +0900

    create vue, axios, sass, mysql, redis environment

---
+++ resources/js/csrf.js
@@ -0,0 +1,12 @@
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+const token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+  window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+  console.error('CSRF token not found');
+}
